b72801e17ad65a00d89bf3c80c1e3530dd146aeb
Mariano D'Agostino <mariano@cuencodigital.com>
2015-09-28 21:46:49 -0300

    Agregados los codigos del cronometro

--- test/Cronometro/CronometroTest.java
+++ test/Cronometro/CronometroTest.java
@@ -7,9 +7,19 @@ import org.junit.Test;
 import uk.co.placona.Cronometro.Cronometro;
 
 public class CronometroTest {
-	@Test
-	public void testHellowWorld(){
-		Cronometro hello = new Cronometro();
-		assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
-	}
+
+  @Test
+  public void testCronometroEnCero(){
+    Cronometro cronometro = new Cronometro();
+    cronometro.reiniciar();
+    assertEquals("Probar el cronometro en cero", "0 minutos 0 segundos", cronometro.mostrarValor());
+  }
+
+  @Test
+  public void testCronometroEnUno(){
+    Cronometro cronometro = new Cronometro();
+    cronometro.incrementar();
+    assertEquals("Probar el cronometro en uno", "0 minutos 1 segundos", cronometro.mostrarValor());
+  }
+
 }
